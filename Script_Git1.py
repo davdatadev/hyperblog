af06d34074165056ae22a6e50c00edddb402554d
David Arenas <darenasz@sura.com.co>
2023-01-24 21:58:04 -0500

    Agregamos la fecha de hoy y calculamos la edad y la mostramos

--- Script_Git1.py
+++ Script_Git1.py
@@ -1,5 +1,12 @@
 import os
-from datetime import date
+from datetime import datetime
+
+hoy = datetime.strftime(datetime.now().date(),"%d-%m-%Y")
+edad = datetime.now() - datetime.strptime("1992-09-04", "%Y-%m-%d")
 print("Hola Mundo!")
 print("Directorio de trabajo:", os.getcwd())
-print(f"La fecha de hoy: {date.today}")
+print(f"La fecha de hoy: {hoy}")
+print(f"Mi nombre es: David")
+print(f"Mi edad es {round(edad.days/365)}")
+
+
