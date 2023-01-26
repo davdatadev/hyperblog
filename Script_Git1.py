5364ad4b37e222063b206d29d795569c801594e9
David Arenas <darenasz@sura.com.co>
2023-01-25 21:29:20 -0500

    Calculo de edad libreria dateutil

--- Script_Git1.py
+++ Script_Git1.py
@@ -1,10 +1,12 @@
 import os
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 hoy = datetime.strftime(datetime.now().date(),"%d-%m-%Y")
-edad = datetime.now() - datetime.strptime("1992-09-04", "%Y-%m-%d")
+#edad = datetime.now() - datetime.strptime("1992-09-04", "%Y-%m-%d")
+edad = relativedelta(datetime.now(), datetime.strptime("1992-09-04", "%Y-%m-%d")).years
 print("Hola Mundo!")
 print("Directorio de trabajo:", os.getcwd())
 print(f"La fecha de hoy: {hoy}")
 print(f"Mi nombre es: David")
-print(f"Mi edad es {round(edad.days/365)}")
+print(f"Mi edad es {edad}")
