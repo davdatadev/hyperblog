0f1d5c1e59031cb8a7dccd01522479ceb864e60d
David Arenas <darenasz@sura.com.co>
2023-01-30 06:50:18 -0500

    Comentario inicial en Script

--- Script_Git1.py
+++ Script_Git1.py
@@ -2,6 +2,7 @@ import os
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
+#Script de muestra
 hoy = datetime.strftime(datetime.now().date(),"%d-%m-%Y")
 #edad = datetime.now() - datetime.strptime("1992-09-04", "%Y-%m-%d")
 edad = relativedelta(datetime.now(), datetime.strptime("1992-09-04", "%Y-%m-%d")).years
