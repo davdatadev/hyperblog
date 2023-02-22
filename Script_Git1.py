d32b3b61e69e5236dc6606779121d2a867ca09e4
David Arenas <darenasz@sura.com.co>
2023-02-21 20:03:09 -0500

    Se agrega un f string para la funcion optener directorio de trabajo

--- Script_Git1.py
+++ Script_Git1.py
@@ -7,7 +7,7 @@ hoy = datetime.strftime(datetime.now().date(),"%d-%m-%Y")
 #edad = datetime.now() - datetime.strptime("1992-09-04", "%Y-%m-%d")
 edad = relativedelta(datetime.now(), datetime.strptime("1992-09-04", "%Y-%m-%d")).years
 print("Hola Mundo!")
-print("Directorio de trabajo:", os.getcwd())
+print(f"Directorio de trabajo: {os.getcwd()}")
 print(f"La fecha de hoy: {hoy}")
 print(f"Mi nombre es: David")
 print(f"Mi edad es {edad}")
