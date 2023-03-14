e8e6bc69a50bc4648566ac5493ef19c5fc94dbae
David Arenas Zapata <darenasz@sura.com.co>
2023-03-13 21:20:55 -0500

    Contribuimos cambiando el Scrip.py por algo más profesional

--- Script_Git1.py
+++ Script_Git1.py
@@ -2,12 +2,18 @@ import os
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-#Script de muestra
-hoy = datetime.strftime(datetime.now().date(),"%d-%m-%Y")
-#edad = datetime.now() - datetime.strptime("1992-09-04", "%Y-%m-%d")
-edad = relativedelta(datetime.now(), datetime.strptime("1992-09-04", "%Y-%m-%d")).years
-print("Hola Mundo!")
-print(f"Directorio de trabajo: {os.getcwd()}")
-print(f"La fecha de hoy: {hoy}")
-print(f"Mi nombre es: David")
-print(f"Mi edad es {edad}")
+def calc_age(bithday):
+    hoy = datetime.strftime(datetime.now().date(),"%d-%m-%Y")
+    edad = relativedelta(datetime.now(), datetime.strptime("1992-09-04", "%Y-%m-%d")).years
+    return edad, hoy
+
+def main():
+    edad, hoy = calc_age("1992-09-04")
+    print("Hola Mundo!")
+    print(f"Directorio de trabajo: {os.getcwd()}")
+    print(f"La fecha de hoy: {hoy}")
+    print(f"Mi nombre es: David")
+    print(f"Mi edad es {edad}")
+
+if __name__ == "__main__":
+    main()
